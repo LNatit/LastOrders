f94061624df914d8848a34dcf90437102ebd5819
Loci_Natit <l_natit@outlook.com>
2024-10-19 00:09:38 +0800

    Clear useless files

---
+++ src/main/java/com/lnatit/lastorders/LastOrders.java
@@ -0,0 +1,8 @@
+package com.lnatit.lastorders;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(LastOrders.MOD_ID)
+public class LastOrders {
+    public static final String MOD_ID = "lastorders";
+}
